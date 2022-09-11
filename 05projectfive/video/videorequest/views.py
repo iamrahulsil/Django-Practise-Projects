5894a9ecbfae9c3e729c29d964982a82fcf5c1df
Rahul Sil <rahul.official.150@gmail.com>
2022-09-11 16:48:45 +0530

    added index view

--- 05projectfive/video/videorequest/views.py
+++ 05projectfive/video/videorequest/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import VideoReq
 
 
 def index(request):
-    return render(request, "videorequest/index.html")
+    videoModel = VideoReq
+    context = {'videos': videoModel}
+    return render(request, "videorequest/index.html", context)
 
 
 def vrform(request):
