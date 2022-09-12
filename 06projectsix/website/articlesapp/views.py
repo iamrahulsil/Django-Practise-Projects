ce6fdfd564f4dbd10e947175af6710cde05b281f
Rahul Sil <rahul.official.150@gmail.com>
2022-09-12 09:40:16 +0530

    project 6

---
+++ 06projectsix/website/articlesapp/views.py
@@ -0,0 +1,17 @@
+#from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView, DetailView
+from .models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "home.html"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "detail.html"
+    context_object_name = "blogs"
