978f4d6851c024a02b6cb682f454014af70355e3
Rahul Sil <rahul.official.150@gmail.com>
2022-09-12 11:56:12 +0530

    create updateview

--- 06projectsix/website/articlesapp/urls.py
+++ 06projectsix/website/articlesapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.ArticleListView.as_view(), name="home"),
     path("article/<int:pk>", views.ArticleDetailView.as_view(), name="blogpage"),
     path("article/new/", views.ArticleCreateView.as_view(), name="new_article"),
+    path("article/<int:pk>/edit",
+         views.ArticleUpdateView.as_view(), name="article_edit"),
 ]
--- 06projectsix/website/articlesapp/views.py
+++ 06projectsix/website/articlesapp/views.py
@@ -3,7 +3,7 @@
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Article
 
 
@@ -25,3 +25,10 @@ class ArticleCreateView(CreateView):
     model = Article
     template_name = "article_new.html"
     fields = "__all__"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = "article_edit.html"
+    fields = "__all__"
+#   fields = ["title", "text"]
