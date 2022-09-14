c3094bd58fcb920b8f0e5e663193b2ce689088f2
Rahul Sil <rahul.official.150@gmail.com>
2022-09-14 10:31:55 +0530

    add decorators to enter a todo task

--- 07projectseven/todoproj/todoapp/views.py
+++ 07projectseven/todoproj/todoapp/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 
+from .models import TodoModel
+from .forms import TodoForm
+
 
 def index(request):
-    return render(request, "todoapp/index.html")
+    mytodo = TodoModel.objects.order_by("pk")
+    form = TodoForm()
+    context = {'mytodo': mytodo, 'form': form}
+    return render(request, "todoapp/index.html", context)
+
+
+@require_POST
+def addNewTodo(request):
+    form = TodoForm(request.POST)
+
+    if form.is_valid():
+        my_new_todo = TodoModel(task=request.POST['text'])
+        my_new_todo.save()
+
+    return redirect('index')
